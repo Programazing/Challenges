6aa8445831639e6395fc6261386b4a6a2ac1b794
Programazing <chris@thatamazingprogrammer.com>
2019-05-17 06:47:36 -0400

    Save current work while I switch computers.

--- OpCode_Challenges/Daily_Programmer.Tests/Add_Digits.Test.cs
+++ OpCode_Challenges/Daily_Programmer.Tests/Add_Digits.Test.cs
@@ -7,8 +7,22 @@ using System.Text;
 namespace Daily_Programmer.Tests
 {
     [TestFixture]
-    class Add_Digits
+    class Add_Digits_Test
     {
+        private readonly Add_Digits Add_Digits;
+
+        public Add_Digits_Test()
+        {
+            Add_Digits = new Add_Digits();
+        }
+
+        [TestCase(1)]
+        public void Input_IsA_NonNegativeNumber(int input)
+        {
+            var sut = Add_Digits.Calculate(input);
+
+            sut.Should().BePositive();
+        }
 
     }
 }
--- OpCode_Challenges/Daily_Programmer/2019/05/16/Add_Digits.cs
+++ OpCode_Challenges/Daily_Programmer/2019/05/16/Add_Digits.cs
@@ -4,7 +4,38 @@ using System.Text;
 
 namespace Daily_Programmer
 {
-    class Add_Digits
+    public class Add_Digits
     {
+        public int Calculate(int input)
+        {
+            ValidateNumber(input);
+
+            var inputArray = ToIntArray(input);
+
+            return 1;
+
+        }
+
+
+        private static int[] ToIntArray(int input)
+        {
+            var output = new List<int>();
+            var charArray = input.ToString().ToCharArray();
+
+            foreach (var item in charArray)
+            {
+                output.Add((int)char.GetNumericValue(item));
+            }
+
+            return output.ToArray();
+        }
+
+        private static void ValidateNumber(int input)
+        {
+            if (input < 0)
+            {
+                throw new ArgumentOutOfRangeException("input", "input must be a non-negative number.");
+            }
+        }
     }
 }
